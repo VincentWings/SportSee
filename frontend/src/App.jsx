45f17d3cde981a279bf871727537be023b930796
Vincent Wings <wingsvincent@gmail.com>
2025-02-02 09:10:26 +0100

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import sportseeLogo from './assets/sportsee.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="#" target="_blank">
+          <img src={sportseeLogo} className="logo" alt="SportSee logo" />
+        </a>
+      </div>
+
+      <h1>SportSee</h1>
+
+      <div className="card">
+        <p>
+          test
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
